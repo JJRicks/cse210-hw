648764561005d99678d9b0f394161a0b0ea19158
Joel Johnson <joel.ricks.johnson@gmail.com>
2024-05-03 23:10:31 -0600

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep4 World!");
+
+        // List<string> words = new List<string>();
+        // words.Add("phone");
+        // words.Add("keyboard");
+        // words.Add("mouse");
+
+        // foreach (string word in words) {
+        //     Console.WriteLine(word);
+        // }
+
+        // Console.WriteLine(words.Count);
+        // for (int i = 0; i < words.Count; i++) {
+        //     Console.WriteLine(words[i]);
+        // }
+
+        List<int> numbersList = new List<int>();
+        int userNumber = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (userNumber != 0) {
+            Console.Write("Enter a number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0){
+                numbersList.Add(userNumber);
+            }
+        }
+        
+        int sum = numbersList.Sum();
+        double average = numbersList.Average();
+        int largest = numbersList.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
+        
+        
     }
 }
