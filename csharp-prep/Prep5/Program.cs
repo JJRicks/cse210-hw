1185e810e8f39134f900f066eafd70e56caa7f7c
Joel Johnson <joel.ricks.johnson@gmail.com>
2024-05-03 23:56:38 -0600

    Finished Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome() {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName(){
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+        static int PromptUserNumber() {
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+        static int SquaredNumber(int userNumber) {
+            return userNumber * userNumber;
+        }
+        static void DisplayResult(string userName, int userNumberSquared){
+            Console.WriteLine($"{userName}, the square of your number is {userNumberSquared}");
+        }
+    
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredUserNumber = SquaredNumber(userNumber);
+        DisplayResult(userName, squaredUserNumber);
+
     }
 }
