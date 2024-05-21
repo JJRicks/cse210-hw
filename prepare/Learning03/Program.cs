5fe3e0e47611dc967ea1c092c5113bdc88d90f2a
Joel Johnson <joel.ricks.johnson@gmail.com>
2024-05-20 21:04:13 -0600

    Completed learning03 objectives

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction0 = new Fraction();
+        Console.WriteLine(fraction0.getTop());
+        Console.WriteLine(fraction0.getBottom());
+        Console.WriteLine(fraction0.GetDecimalValue());
+        Console.WriteLine(fraction0.GetFractionString());
+
+
+        Fraction fraction1 = new Fraction(2);
+        Console.WriteLine(fraction1.getTop());
+        Console.WriteLine(fraction1.getBottom());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction1.GetFractionString());
+
+        Fraction fraction2 = new Fraction(5, 6);
+        Console.WriteLine(fraction2.getTop());
+        Console.WriteLine(fraction2.getBottom());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFractionString());
+
     }
+
+
 }
