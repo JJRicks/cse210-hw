6c2a91693f9b02913a80d863a468652e5dd8dd63
Joel Johnson <joel.ricks.johnson@gmail.com>
2024-05-27 18:19:47 -0600

    initial menu

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.Clear();
+        while (true) {
+            
+            Console.WriteLine("Welcome to scripture memorizer. Please select from the following options: (1, 2, 3 or quit)");
+            Console.WriteLine("1: Add a scripture to the vault");
+            Console.WriteLine("2: Display a scripture for memorization");
+            Console.WriteLine("3 or quit: Exit the program");
+            Console.Write("Selection: ");
+            
+            string userChoice = Console.ReadLine();
+            
+            switch (userChoice) {
+                case "1": // add a scripture to the vault
+                    
+                    break; 
+                case "2": // display a scripture for memorization
+                    break;
+                case "3": // quit
+                    break;
+                case "quit": // other way to quit
+                    break;
+                default:
+                    Console.WriteLine("\nNot a vaild menu selection, please try again.\n");
+                    break;
+            }
+        }
     }
 }
